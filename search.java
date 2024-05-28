8ad6ee79cd8d21e9557b2bcb2a36f06882d7a0ed
Angelina3405 <165631644+Angelina3405@users.noreply.github.com>
2024-05-29 00:39:21 +0500

    search.java

---
+++ search.java
@@ -0,0 +1,11 @@
+public abstract class Search<V> {
+    protected WeightedGraph<V> graph;
+    protected Vertex<V> start;
+
+    public Search(WeightedGraph<V> graph, Vertex<V> start) {
+        this.graph = graph;
+        this.start = start;
+    }
+
+    public abstract void execute();
+}
